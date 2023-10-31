785b5fe4e276bcea76785168688f9e3b961f0594
samirametin <115466868+samirametin@users.noreply.github.com>
2023-10-31 21:34:51 +0400

    fixing functions

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from databasa import resources
 from math import *
 
 
-def checking_resources(coffe_name):
-    ingredients = MENU[coffe_name]["ingredients"]
-    for item in ingredients:
-        if resources[item] < ingredients[item]:
+def checking_resources(coffe_ingredient):
+    for item in coffe_ingredient:
+        if resources[item] <= ingredients[item]:
             print(f"Sorry there is not enough {item}")
-        else:
-            resources[item] -= ingredients[item]
-
+            return False
+    return True
 
 
 def getting_coins():
@@ -19,31 +17,44 @@ def getting_coins():
     dimes = int(input("How many dimes?: "))
     nickels = int(input("How many nickles?: "))
     pennies = int(input("How many pennies?: "))
-    total = quarters * 0.25 + dimes * 0.1 + nickels * 0.05 + pennies * 0.01
+    total = round((quarters * 0.25 + dimes * 0.1 + nickels * 0.05 + pennies * 0.01), 2)
     return total
 
 
-def checking_price(money,coffee_name):
+def checking_price(money, coffee_name):
     coffee_price = MENU[coffee_name]["cost"]
     if money >= coffee_price:
         change = round(money - coffee_price, 2)
-
-        return f"Here is ${change} change.\nHere is your {coffee_name}! Enjoy!"
+        print(f"Here is ${change} change.")
+        global budget
+        budget += coffee_price
+        return True
     else:
-        return "Sorry that's not enough money. Money refunded."
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+
+def make_coffee(coffee_name, order_ingredients):
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {coffee_name}! Enjoy!")
 
 
 budget = 0
-def coffe_machine():
+is_on = True
+while is_on:
     order = input("What would you like? (espresso/latte/cappuccino): ")
-    if order == "report":
+    if order == "off":
+        is_on = False
+    elif order == "report":
         print(f"Water: {resources['water']}ml")
         print(f"Milk: {resources['milk']}ml")
         print(f"Coffee: {resources['coffee']}gr")
         print(f"Money: ${budget}")
-    checking_resources(order)
-    your_money = getting_coins()
+    else:
+        ingredients = MENU[order]["ingredients"]
+        if checking_resources(ingredients):
+            your_money = getting_coins()
+            if checking_price(your_money, order):
+                make_coffee(order, ingredients)
 
-    money_procces = checking_price(your_money, order)
-    print(your_money)
-    print(money_procces)
